f3480dea855ee648a848dd993b842d17337b1c0b
Philip Hayes <phayes@redhat.com>
2020-08-26 11:46:29 -0400

    Updated version of cloudevents

--- samples/knative-service/index.js
+++ samples/knative-service/index.js
@@ -1,18 +1,18 @@
 const app = require('express')();
 const {Receiver} = require("cloudevents");
 
-app.use((req, res, next) => {
-  let data = "";
+// app.use((req, res, next) => {
+//   let data = "";
 
-  //req.setEncoding("utf8");
-  req.on("data", function(chunk) {
-      data += chunk;
-  });
-  req.on("end", function() {
-      req.body = data;
-      next();
-  });
-});
+//   //req.setEncoding("utf8");
+//   req.on("data", function(chunk) {
+//       data += chunk;
+//   });
+//   req.on("end", function() {
+//       req.body = data;
+//       next();
+//   });
+// });
 app.post('/', (req, res) => {
   
   try {
